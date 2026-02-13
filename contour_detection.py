f442cb410c67481c216f14ef3363ea3b2bdb2c3b
Sonali Balekundri <sonalibalekundri111@gmail.com>
2026-02-13 13:47:54 +0000

    Update contour_detection.py

--- contour_detection.py
+++ contour_detection.py
@@ -7,7 +7,7 @@ They are not the same as edges.
 Contours are useful tools when you get into shape
 analysis and object detection.'''
 
-img=cv.imread('retina26.jpg')
+img=cv.imread('images/retina26.jpg')
 cv.imshow('eye', img)
 
 '''Using blank image so that we can draw the 
@@ -43,6 +43,7 @@ cv.drawContours(blank,contours,-1,(0,0,255),1)
 cv.imshow('Contours Drawn',blank)
 
 cv.waitKey(0) 
+
 
 
 
